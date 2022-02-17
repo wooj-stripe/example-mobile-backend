bf680aa06ebbd1c409cb7b4f067c2c22e55dd5b1
John Woo <99628984+wooj-stripe@users.noreply.github.com>
2022-02-16 19:34:47 -0800

    Update web.rb

--- web.rb
+++ web.rb
@@ -357,7 +357,7 @@ def price_lookup(product)
 end
 
 def calculate_price(products, shipping)
-  amount = 1099  # Default amount.
+  amount = 6099  # Default amount.
 
   if products
     amount = products.reduce(0) { | sum, product | sum + price_lookup(product) }
